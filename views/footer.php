e54e701c7baa06141beb726d420f790b1b4abca3
Kazume <mcwicio@gmail.com>
2024-11-12 10:03:10 +0100

    Adding footer/header

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,12 @@
-<!-- views/footer.php -->
-<footer>
-    <!-- Zawartość stopki -->
-</footer>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <div style="background-color: red;">
+        AAAAAAAAAAAAAAA
+    </div>
 </body>
 </html>
--- views/header.php
+++ views/header.php
@@ -9,12 +9,14 @@
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/styles.css">
 </head>
 <body>
-    <header>
-        <nav>
-            <ul>
-                <li><a href="<?php echo BASE_URL; ?>/index.php">Strona główna</a></li>
-                <li><a href="<?php echo BASE_URL; ?>/blog.php">Blog</a></li>
-                <li><a href="<?php echo BASE_URL; ?>/about.php">O mnie</a></li>
-            </ul>
+<header class="header">
+        <a href="#" class="logo">{Portfolio} </a>
+
+        <nav class="navbar">
+            <a href="#home" style="--i:1;" class="active">Home</a>
+            <a href="#about" style="--i:2;">About</a>
+            <a href="#skill" style="--i:3;">Skills</a>
+            <a href="#project" style="--i:4;">Projects</a>
+            <a href="#contact" style="--i:5;">Contact</a>
         </nav>
     </header>
