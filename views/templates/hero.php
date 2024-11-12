a0929f641289e828eb596ec5bcd8b55839c122b3
Kazume <mcwicio@gmail.com>
2024-11-12 10:02:18 +0100

    Adding Hero

---
+++ views/templates/hero.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio | Sai</title>
+
+    <link rel="icon" type="png|image" href="/IMAGES/sailogo.png">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/styles.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
+
+</head>
+
+<body>
+
+    <section id="hero" class="hero">
+
+        <div class="hero-content" data-scroll data-scroll-speed="-5">
+            <h3 class="mu">ree</h3>
+            <h1>Jakub Witek</h1>
+            <h3>And I'm a <span class="pro-text">
+                    <script>
+                        var typed = new Typed(".pro-text", {
+                            strings: [
+                                "Test1",
+                                "womp womp",
+                                "Test2",
+                                "womp womp",
+                                "Test3",
+                            ],
+                            typeSpeed: 100,
+                            backSpeed: 100,
+                            backDelay: 1000,
+                            loop: true
+                        });
+                    </script>
+                </span></h3>
+
+            <p> &emsp; &emsp; <br> <br>Test Test Test (test) <br> <br>
+                &emsp; &emsp;Test Test Test (test) <br> <br>
+                &emsp; &emsp;Test Test Test (test)
+            </p>
+
+            <div class="hero-sci">
+                <a href="https://github.com/SAI127001" target="_blank"><i class='bx bxl-github'></i></a>
+                <a href="https://www.linkedin.com/in/terukula-sai/" target="_blank"><i class='bx bxl-linkedin'></i></a>
+                <a href="https://wa.me/9987886552" target="_blank"><i class='bx bxl-whatsapp'></i></a>
+                <a href="https://www.instagram.com/sai_127.0.0.1/" target="_blank"><i class='bx bxl-instagram'></i></a>
+            </div>
+            <a href="" class="btn-box" download="Sai CV/Resume">Download CV</a>
+        </div>
+
+        <div class="hero-img">
+            <div class="glowing-circle">
+                <span></span>
+                <span></span>
+                <div class="image">
+                    <img src="../public/IMAGES/Ork.png" alt="REEEEEEEEEEEEEEEEE">
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+</body>
+
+</html>
